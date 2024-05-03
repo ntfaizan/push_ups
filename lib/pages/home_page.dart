9c5f3ca223cfd40e462ce6b46015733d4a690c76
Faizan Abbas <faizan.abbas@nutech.edu.pk>
2024-05-03 11:25:42 +0500

    sqlite

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../providers/sql_helper.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -8,11 +10,69 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int counter = 0;
+
+  @override
+  void initState() {
+    initData();
+    super.initState();
+  }
+
+  Future<void> initData() async {
+    counter = await SQLHelper.sql.getCounter();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [],
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text('$counter'),
+            const Text('Push Ups'),
+            FilledButton(
+              onPressed: () async {
+                setState(() {
+                  counter++;
+                });
+                if (await SQLHelper.sql.counterExists()) {
+                  await SQLHelper.sql.updateCounter(counter);
+                } else {
+                  SQLHelper.sql.createCounter();
+                }
+              },
+              child: const Text('+'),
+            ),
+            FilledButton(
+              onPressed: () async {
+                setState(() {
+                  counter--;
+                });
+                if (await SQLHelper.sql.counterExists()) {
+                  await SQLHelper.sql.updateCounter(counter);
+                } else {
+                  SQLHelper.sql.createCounter();
+                }
+              },
+              child: const Text('-'),
+            ),
+            FilledButton(
+              onPressed: () async {
+                setState(() {
+                  counter = 0;
+                });
+                if (await SQLHelper.sql.counterExists()) {
+                  await SQLHelper.sql.updateCounter(counter);
+                } else {
+                  SQLHelper.sql.createCounter();
+                }
+              },
+              child: const Text('Reset'),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/providers/sql_helper.dart
@@ -0,0 +1,57 @@
+import 'package:sqflite/sqflite.dart';
+
+class SQLHelper {
+  static final SQLHelper sql = SQLHelper._init();
+  SQLHelper._init();
+
+  Future<Database> get database async {
+    return await _initDB();
+  }
+
+  Future<Database> _initDB() async {
+    // final dbPath = await getDatabasesPath();
+    // final path = join(dbPath, 'counter.db');
+    return await openDatabase(
+      'counter.db',
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute(
+          'CREATE TABLE IF NOT EXISTS counter(count INTEGER PRIMARY KEY)',
+        );
+        // await db.execute(
+        //   'CREATE TABLE IF NOT EXISTS counter(count INTEGER PRIMARY KEY)',
+        // );
+      },
+    );
+  }
+
+  Future<bool> counterExists() async {
+    final db = await database;
+    final result = await db.rawQuery("SELECT * FROM counter");
+    if (result.isEmpty) {
+      return false;
+    }
+    return true;
+  }
+
+  void createCounter() async {
+    final db = await database;
+    await db.rawInsert("INSERT INTO counter(count) VALUES(?)", [0]);
+    db.close();
+  }
+
+  Future<void> updateCounter(int value) async {
+    final db = await database;
+    await db.rawUpdate("UPDATE counter SET count=?", [value]);
+    db.close();
+  }
+
+  Future<int> getCounter() async {
+    final db = await database;
+    final result = await db.rawQuery("SELECT * FROM counter");
+    if (result.isNotEmpty) {
+      return result[0]['count'] as int;
+    }
+    return 0;
+  }
+}
